539ff96b570b5771eea8db9f7174838aa80a59bb
K <kayo@illumium.org>
2020-08-02 14:19:05 +0500

    Added LDC2 D-lang compiler

--- default.nix
+++ default.nix
@@ -1,4 +1,5 @@
 self: super: with super; rec {
+  # LLVM-based xtensa toolchain
   llvmPackages_10_xtensa = callPackage ./llvm/10 ({
     inherit (stdenvAdapters) overrideCC;
     buildLlvmTools = buildPackages.llvmPackages_10_xtensa.tools;
@@ -8,7 +9,14 @@ self: super: with super; rec {
   });
   clang_10_xtensa = llvmPackages_10_xtensa.clang;
   llvm_10_xtensa = llvmPackages_10_xtensa.llvm;
+
   llvmPackages_xtensa = llvmPackages_10_xtensa;
   clang_xtensa = clang_10_xtensa;
   llvm_xtensa = llvm_10_xtensa;
+
+  # LLVM-based D-lang toolchain
+  ldc_xtensa = callPackage ./ldc {
+    llvm_8 = llvm_10_xtensa;
+  };
+
 }
---
+++ ldc/default.nix
@@ -0,0 +1,4 @@
+import ./generic.nix {
+  version = "1.22.0";
+  ldcSha256 = "12497nlvda2a8wd6ffzn7xm2s17z0vrbazw4al6mn8pvc6nfqsw6";
+}
